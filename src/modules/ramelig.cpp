1599e1668d535cc602402ba6acbf21a78778d853
Johan Vanbockryck <johan@not-things.com>
2025-07-01 22:57:56 +0200

    Compiles, but is not right

--- src/modules/ramelig.cpp
+++ src/modules/ramelig.cpp
@@ -5,15 +5,19 @@
 
 extern Model* modelRamelig;
 
-enum RameligChanceDistributionIndex {
-	JUMP = 0,
-	MOVE = 1,
-	UP_ONE = 2,
+enum RameligStepActions {
+	RANDOM_JUMP = 0,
+	RANDOM_MOVE = 1,
+	AROUND_CURRENT = 2,
+
 	UP_TWO = 3,
-	REMAIN = 4,
-	DOWN_ONE = 5,
-	DOWN_TWO = 6
+	UP_ONE = 4,
+	REMAIN = 5,
+	DOWN_ONE = 6,
+	DOWN_TWO = 7
 };
+
+#define TO_MOVE_INDEX(stepAction) stepAction - UP_TWO
 
 struct RameligData {
 	std::array<std::vector<int>, 12> scales;
@@ -26,34 +30,46 @@ struct RameligData {
 	float randomMoveChance; // The chance that the melody line will move to another position and stay there
 
 	float moveUpChance; // How likely it is that a move will go up
-	float moveDownChance; // How likely it is that a move will go down
 	float remainChance; // How likely it is that the melody won't move
+	float moveDownChance; // How likely it is that a move will go down
 
-	float moveOneChance; // How likely it is that a move will be one position
-	float moveTwoChance; // How likely it is that a move will be two pisitions
+	float moveTwoChance; // How likely it is that a move will be two positions
 
-	std::array<float, 7> chanceDistribution; // The distibution of the different chance outcomes, indexed by RameligChanceDistributionIndex
+	std::array<float, 3> stepDistribution; // The distribution of possible step actions: random jump, random move, or move around current position
+	std::array<float, 5> moveDistribution; // The distribution of possible movements around the curren position: up two, up one, remain, down one and down two
 
-	void calculateDistribution() {
-		chanceDistribution[JUMP] = randomJumpChance;
-		chanceDistribution[MOVE] = chanceDistribution[JUMP] + randomMoveChance;
-		
-		chanceDistribution[UP_ONE] = chanceDistribution[MOVE] + (moveUpChance * 10 / moveOneChance);
-		chanceDistribution[UP_TWO] = chanceDistribution[MOVE] + moveUpChance;
+	void calculateDistributions() {
+		stepDistribution[RANDOM_JUMP] = randomJumpChance;
+		stepDistribution[RANDOM_MOVE] = stepDistribution[RANDOM_JUMP] + randomMoveChance;
+		stepDistribution[AROUND_CURRENT] = stepDistribution[RANDOM_MOVE] + moveUpChance + remainChance + moveDownChance;
 
-		chanceDistribution[REMAIN] = chanceDistribution[UP_TWO] + remainChance;
-
-		chanceDistribution[DOWN_ONE] = chanceDistribution[REMAIN] + (moveDownChance * 10 / moveOneChance);
-		chanceDistribution[DOWN_TWO] = chanceDistribution[REMAIN] + moveDownChance;
+		moveDistribution[TO_MOVE_INDEX(UP_TWO)] = moveUpChance * moveTwoChance / 10;
+		moveDistribution[TO_MOVE_INDEX(UP_ONE)] = moveUpChance;
+		moveDistribution[TO_MOVE_INDEX(REMAIN)] = moveUpChance + remainChance;
+		moveDistribution[TO_MOVE_INDEX(DOWN_ONE)] = moveDistribution[TO_MOVE_INDEX(REMAIN)] + (moveDownChance * (10 - moveTwoChance) / 10);
+		moveDistribution[TO_MOVE_INDEX(DOWN_TWO)] = moveDistribution[TO_MOVE_INDEX(REMAIN)] + moveDownChance;
 	}
 
-	RameligChanceDistributionIndex determineChance(std::minstd_rand &generator) {
-		float chance = std::uniform_real_distribution<float>(0.f, chanceDistribution[7])(generator);
-		RameligChanceDistributionIndex result = DOWN_TWO;
+	RameligStepActions determineChance(std::minstd_rand &generator) {
+		float chance = std::uniform_real_distribution<float>(0.f, stepDistribution[AROUND_CURRENT])(generator);
 
-		for (unsigned int i = 0; i < 7; i++) {
-			if (chanceDistribution[i] >= chance) {
-				result = static_cast<RameligChanceDistributionIndex>(i);
+		RameligStepActions result = AROUND_CURRENT;
+		for (unsigned int i = 0; i < 3; i++) {
+			if (stepDistribution[i] > chance) {
+				result = static_cast<RameligStepActions>(i);
+				break;
+			}
+		}
+
+		// TODO: Bias upwards or downwards when moving near the edges of the allowed values
+		if (result == AROUND_CURRENT) {
+			chance -= randomJumpChance;
+			result = DOWN_TWO;
+			for (unsigned int i = 0; i < 5; i++) {
+				if (moveDistribution[i] > chance) {
+					result = static_cast<RameligStepActions>(i + 3);
+					break;
+				}
 			}
 		}
 
@@ -61,7 +77,7 @@ struct RameligData {
 	}
 };
 
-float quantize(float value, std::vector<int> indices, float lowerLimit, float upperLimit) {
+float quantize(float value, std::vector<int>& indices, float lowerLimit, float upperLimit, std::vector<float>& notes) {
 	float oct;
 	float fract;
 	if (value < 0) {
@@ -71,21 +87,16 @@ float quantize(float value, std::vector<int> indices, float lowerLimit, float up
 		fract = std::modf(value, &oct);
 	}
 
-	float q = -1.f;
+	float note = -1.f + notes.back();
 	for (int i = 0; i < indices.size(); i++) {
 		if (fract <= notes[indices[i]]) {
-			q = notes[indices[i]];
+			note = notes[indices[i]];
 		} else {
 			break;
 		}
 	}
 
-	if (q == -1.f) {
-		whole--;
-		q = notes[indices.back()];
-	}
-
-	return whole + q;
+	return oct + note;
 }
 
 
@@ -109,16 +120,18 @@ RameligModule::RameligModule() {
 	m_data->scales[0] = { 0, 2, 4, 7, 9 };
 	m_data->currentScale = 0;
 
-	m_data->randomJumpChance = 0.1f;
-	m_data->randomMoveChance = 0.1f;
+	m_data->currentOctave = 0;
+	m_data->currentScaleIndex = 0;
+
+	m_data->randomJumpChance = 0.2f;
+	m_data->randomMoveChance = 0.2f;
 	m_data->moveUpChance = 0.9f;
-	m_data->remainChance = 0.2f;
+	m_data->remainChance = 0.3f;
 	m_data->moveDownChance = 0.9f;
 
-	m_data->moveOneChance = 0.7f;
-	m_data->moveOneChance = 0.3f;
+	m_data->moveTwoChance = 0.3f;
 
-	m_data->calculateDistribution();
+	m_data->calculateDistributions();
 }
 
 RameligModule::~RameligModule() {
@@ -131,60 +144,138 @@ void RameligModule::process(const ProcessArgs& args) {
 		float lowerLimit = inputs[IN_LOWER_LIMIT].getVoltage();
 
 		switch (m_data->determineChance(m_generator)) {
-			case JUMP:
-			case MOVE:
-			case UP_ONE:
-			case UP_TWO:
-			case REMAIN:
-			case DOWN_ONE:
-			case DOWN_TWO:
-		}
-		
-		if (std::uniform_real_distribution<float>(0.f, 100.f)(m_generator) < m_data->randChance) {
-			float value = std::uniform_real_distribution<float>(lowerLimit, upperLimit)(m_generator);
-			value = quantize(value, m_data->indices);
-			outputs[OUT_CV].setVoltage(value);
+			case RANDOM_JUMP:
+			case RANDOM_MOVE: {
+				float value = std::uniform_real_distribution<float>(lowerLimit, upperLimit)(m_generator);
+				value = quantize(value, m_data->scales[m_data->currentScale], lowerLimit, upperLimit, m_notes);
+				outputs[OUT_CV].setVoltage(value);
 
-			float whole;
-			float fract = std::modf(value, &whole);
-			m_data->oct = (int) whole;
-			m_data->index = 0;
-			for (int i = 0; i < m_data->indices.size(); i++) {
-				if (notes[m_data->indices[i]] == fract) {
-					m_data->index = i;
-					break;
+				float whole;
+				float fract = std::modf(value, &whole);
+				m_data->currentOctave = (int) whole;
+				m_data->currentScaleIndex = 0;
+				for (int i = 0; i < m_data->scales[m_data->currentScaleIndex].size(); i++) {
+					if (m_notes[m_data->scales[m_data->currentScaleIndex][i]] == fract) {
+						m_data->currentScaleIndex = i;
+						break;
+					}
 				}
+				break;
 			}
-		} else {
-			int oct = m_data->oct;
-			int index = m_data->index;
-
-			float dist = m_distribution(m_generator);
-
-			if (dist > 2.f) {
-				index++;
-				if (index >= m_data->indices.size()) {
+			case UP_TWO: {
+				int oct = m_data->currentOctave;
+				int index = m_data->currentScaleIndex + 1;
+				if (index >= m_data->scales[m_data->currentScale].size()) {
 					oct++;
 					index = 0;
 				}
-			} else if (dist > 1.f) {
-				index--;
-				if (index < 0) {
-					oct--;
-					index = m_data->indices.size() - 1;
+				float result = (float) oct + m_notes[m_data->scales[m_data->currentScale][index]];
+				if (result > upperLimit) {
+					result = (float) m_data->currentOctave + m_notes[m_data->scales[m_data->currentScale][m_data->currentScaleIndex]];
+				} else {
+					m_data->currentOctave = oct;
+					m_data->currentScaleIndex = index;
 				}
 			}
-
-			float result = (float) oct + notes[m_data->indices[index]];
-			if ((result > upperLimit) || (result < lowerLimit)) {
-				result = (float) m_data->oct + notes[m_data->indices[m_data->index]];
-			} else {
-				m_data->oct = oct;
-				m_data->index = index;
+			case UP_ONE: {
+				int oct = m_data->currentOctave;
+				int index = m_data->currentScaleIndex + 1;
+				if (index >= m_data->scales[m_data->currentScale].size()) {
+					oct++;
+					index = 0;
+				}
+				float result = (float) oct + m_notes[m_data->scales[m_data->currentScale][index]];
+				if (result > upperLimit) {
+					result = (float) m_data->currentOctave + m_notes[m_data->scales[m_data->currentScale][m_data->currentScaleIndex]];
+				} else {
+					m_data->currentOctave = oct;
+					m_data->currentScaleIndex = index;
+				}
+				break;
 			}
-
-			outputs[OUT_CV].setVoltage(result);
+			case REMAIN: {
+				break;
+			}
+			case DOWN_TWO: {
+				int oct = m_data->currentOctave;
+				int index = m_data->currentScaleIndex - 1;
+				if (index  < 0) {
+					oct--;
+					index = m_data->scales[m_data->currentScale].size() - 1;
+				}
+				float result = (float) oct + m_notes[m_data->scales[m_data->currentScale][index]];
+				if (result < lowerLimit) {
+					result = (float) m_data->currentOctave + m_notes[m_data->scales[m_data->currentScale][m_data->currentScaleIndex]];
+				} else {
+					m_data->currentOctave = oct;
+					m_data->currentScaleIndex = index;
+				}
+			}
+			case DOWN_ONE: {
+				int oct = m_data->currentOctave;
+				int index = m_data->currentScaleIndex - 1;
+				if (index  < 0) {
+					oct--;
+					index = m_data->scales[m_data->currentScale].size() - 1;
+				}
+				float result = (float) oct + m_notes[m_data->scales[m_data->currentScale][index]];
+				if (result < lowerLimit) {
+					result = (float) m_data->currentOctave + m_notes[m_data->scales[m_data->currentScale][m_data->currentScaleIndex]];
+				} else {
+					m_data->currentOctave = oct;
+					m_data->currentScaleIndex = index;
+				}
+			}
 		}
+
+		float result = (float) m_data->currentOctave + m_notes[m_data->scales[m_data->currentScale][m_data->currentScaleIndex]];
+		outputs[OUT_CV].setVoltage(result);
+		
+		// if (std::uniform_real_distribution<float>(0.f, 100.f)(m_generator) < m_data->randChance) {
+		// 	float value = std::uniform_real_distribution<float>(lowerLimit, upperLimit)(m_generator);
+		// 	value = quantize(value, m_data->indices);
+		// 	outputs[OUT_CV].setVoltage(value);
+
+		// 	float whole;
+		// 	float fract = std::modf(value, &whole);
+		// 	m_data->oct = (int) whole;
+		// 	m_data->index = 0;
+		// 	for (int i = 0; i < m_data->indices.size(); i++) {
+		// 		if (notes[m_data->indices[i]] == fract) {
+		// 			m_data->index = i;
+		// 			break;
+		// 		}
+		// 	}
+		// } else {
+		// 	int oct = m_data->oct;
+		// 	int index = m_data->index;
+
+		// 	float dist = m_distribution(m_generator);
+
+		// 	if (dist > 2.f) {
+		// 		index++;
+		// 		if (index >= m_data->indices.size()) {
+		// 			oct++;
+		// 			index = 0;
+		// 		}
+		// 	} else if (dist > 1.f) {
+		// 		index--;
+		// 		if (index < 0) {
+		// 			oct--;
+		// 			index = m_data->indices.size() - 1;
+		// 		}
+		// 	}
+
+		// 	float result = (float) oct + notes[m_data->indices[index]];
+		// 	if ((result > upperLimit) || (result < lowerLimit)) {
+		// 		result = (float) m_data->oct + notes[m_data->indices[m_data->index]];
+		// 	} else {
+		// 		m_data->oct = oct;
+		// 		m_data->index = index;
+		// 	}
+
+		// 	outputs[OUT_CV].setVoltage(result);
+		// }
 	}
 }
 
