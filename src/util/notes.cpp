09e7e2a3878d5a9b89d50d44f5a161e6d6798965
Johan Vanbockryck <johan@not-things.com>
2025-07-10 23:32:03 +0200

    Updated Solim limit displays to show nearest note instead of next higher note (#16)

--- src/util/notes.cpp
+++ src/util/notes.cpp
@@ -1,5 +1,6 @@
 #include "util/notes.hpp"
 #include <cctype>
+#include <cmath>
 
 // Take the letter of the note in lowercase, subtract 'a' from it, take the value in this array at that position
 // => the result can be multiplied with 1/12 to get the matchin 1V/oct value.
@@ -8,4 +9,40 @@ const int note_to_index [] = { 9, 11, 0, 2, 4, 5, 7 };
 int noteNameToIndex(char noteName) {
 	char n = tolower(noteName);
 	return note_to_index[(n - 'a')];
+}
+
+int voltageToChromaticIndex(float voltage) {
+	float n;
+	float note = std::modf(voltage, &n);
+	while (note < 0.f) {
+		note++;
+	}
+
+	if (note < (1.f / 24)) {
+		return 0;
+	} else if (note < (1.f / 12) + (1.f / 24)) {
+		return 1;
+	} else if (note < (2.f / 12) + (1.f / 24)) {
+		return 2;
+	} else if (note < (3.f / 12) + (1.f / 24)) {
+		return 3;
+	} else if (note < (4.f / 12) + (1.f / 24)) {
+		return 4;
+	} else if (note < (5.f / 12) + (1.f / 24)) {
+		return 5;
+	} else if (note < (6.f / 12) + (1.f / 24)) {
+		return 6;
+	} else if (note < (7.f / 12) + (1.f / 24)) {
+		return 7;
+	} else if (note < (8.f / 12) + (1.f / 24)) {
+		return 8;
+	} else if (note < (9.f / 12) + (1.f / 24)) {
+		return 9;
+	} else if (note < (10.f / 12) + (1.f / 24)) {
+		return 10;
+	} else if (note < (11.f / 12) + (1.f / 24)) {
+		return 11;
+	} else {
+		return 0;
+	}
 }
